cd4c75ee80a70bada5f4889cf5dcb143699e6e27
chaeminsoo <chaems97@gmail.com>
2021-02-25 18:04:12 +0900

    Feb/25/2021

--- chap8q14.py
+++ chap8q14.py
@@ -3,6 +3,27 @@ collect string, if there are repeated elements, return number of repeated time
 ex) aaabbcccccca -> a3b2c6a1
 '''
 
-inputwd = input('enter a word: ')
+inputwd = input('enter a word: ') #aaabbcccccca
 
-wdlist = list(inputwd)
+wdlist = list(inputwd) #[aaabbcccccca]
+new_wdlist=[]
+index = 0
+
+while True:
+    new_wdlist.append(wdlist[index])
+    count =1
+    n = 1
+    try:
+        while wdlist[index] == wdlist[index+n]:
+            count+=1
+            n+=1
+    except IndexError:
+        pass
+
+    new_wdlist.append(count)
+    index += count
+    if index >= len(wdlist):
+        break
+
+result=''.join(map(str,new_wdlist))
+print('result: ',result)
---
+++ chap8q15.py
@@ -0,0 +1,23 @@
+#입력 -> 분석 -> true/false
+#0123456789 -> Ture, 9685471230 -> Ture, 6328 -> false, 0112346789 -> false
+
+import re
+
+def DupNums(num):
+    p = re.compile(r'\d+\s')
+    refnum = p.findall(num)
+    result =[]
+    for element in refnum:
+        cnt=0      
+        for n in list(range(0,10)):
+            if element.count(str(n))==1: cnt+=1
+            else: break                
+        if cnt == 10: result.append('Ture')
+        else: result.append('False')                
+    return ' '.join(result)
+
+if __name__=='__main__':
+    inputnum = input('enter the numbers: ')
+    #0123456789 01234 01234567890 6789012345 012322456789
+    print(DupNums(inputnum))
+    #True False False True False
